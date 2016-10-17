13b500b0d65a4892adb1e2368888b907d2c9d919
coreyTheDev <coreyzanotti@gmail.com>
2016-10-17 16:26:15 -0400

    Implementing first network calls to ISS endpoint.

--- ISSLocator/FirstViewController.swift
+++ ISSLocator/FirstViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
+import SwiftyJSON
 
 /*
  
  *Required Features*:
  
- - Detect the user's current location - 1 hour
+ ******** Detect the user's current location - 1 hour -
  - Show the user the next time the ISS will pass over their location - 1 hour
  - Send the user a push notification when the ISS is overhead - 30 min
  
@@ -43,9 +45,35 @@ class FirstViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let firstLocation = locations.first {
             usersLocation = firstLocation
+            print("the users location is lat: \(usersLocation?.coordinate.latitude) and long: \(usersLocation?.coordinate.longitude)")
+            self.getISSETA()
         }
     }
 
-
+    //MARK: Networking methods
+    func getISSETA(){
+        guard let lat = self.usersLocation?.coordinate.latitude else { return }
+        guard let lon = self.usersLocation?.coordinate.longitude else { return }
+        let issPassURL = "http://api.open-notify.org/iss-pass.json?lat=\(lat)&lon=\(lon)"
+        
+        Alamofire.request(issPassURL).responseJSON { response in
+            print("call finished")
+            
+//            let jsonData = JSON(response.result.value!)
+//            
+//            print("JSON: \(jsonData)")
+            //loop through each element in JSON and add to an array
+//            var arrayForAdId = self.adEntryDictionary[searchBarText]
+//            if arrayForAdId == nil {
+//                arrayForAdId = [AppIdResponse]()
+//            }
+//            for (_, subJSON): (String, JSON) in jsonData {
+//                let advertisingEntry = AppIdResponse(advertiserId: subJSON["advertiser_id"].string!, ymd: subJSON["ymd"].string!, numClicks: subJSON["num_clicks"].int!, numImpressions: subJSON["num_impressions"].int!)
+//                arrayForAdId?.append(advertisingEntry)
+//            }
+            
+            
+        }
+    }
 }
 
