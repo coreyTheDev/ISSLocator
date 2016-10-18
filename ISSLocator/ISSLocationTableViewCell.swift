16b0bd7469604db620ec52d8f6738b66a8386160
coreyTheDev <coreyzanotti@gmail.com>
2016-10-17 20:29:51 -0400

    adding main table view cell.

---
+++ ISSLocator/ISSLocationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ISSLocationTableViewCell.swift
+//  ISSLocator
+//
+//  Created by Corey Zanotti on 10/17/16.
+//  Copyright © 2016 Corey Zanotti. All rights reserved.
+//
+
+import UIKit
+
+class ISSLocationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
